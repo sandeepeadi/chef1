db23b6d71714820b0cc4d2c4ebfa9ef85de48906
Sandeep Eadi <Sandeep Eadi>
2016-08-08 16:49:45 +0530

    8 AUG 16:49

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,2 @@
+default["apache"]["sites"]["sandeepeadi1"] = { "port" => 80, "domain" => "sandeepeadi1.mylabserver.com" }
+default["apache"]["sites"]["sandeepeadi1b"] = { "port" => 80, "domain" => "sandeepeadi1b.mylabserver.com" }
